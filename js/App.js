283a7f866065bd7e937bf44797f01062a789c85d
Grace <grace_lee1@brown.edu>
2021-04-22 22:34:44 -0400

    added animations to front page, converting to long-scroll site

---
+++ js/App.js
@@ -0,0 +1,61 @@
+window.onload = () => {
+            let content = document.getElementsByClassName("content")[0];
+            let home = document.getElementsByClassName("home")[0];
+            let opacity = 0;
+            let intervalID = setInterval(function() {
+
+                if (opacity < 1) {
+                    opacity = opacity + 0.15
+                    if (content !== undefined) {
+                      content.style.opacity = opacity;
+                    }
+
+                    if (home !== undefined) {
+                      home.style.opacity = opacity;
+                    }
+                } else {
+                    clearInterval(intervalID);
+                }
+            }, 85);
+
+            let still = document.getElementsByClassName('float-on-hover');
+
+            for (let i = 0; i < still.length; i++) {
+
+              still[i].addEventListener('mouseover', () => {
+
+                still[i].animate(
+                  // keyframe
+                  [
+                    { transform: 'translateY(0)' },
+                    { transform: 'translateY(-6px)' },
+                    { transform: 'translateY(0)' }
+                  ], {
+                    // timing options
+                    duration: 700,
+                    iterations: 1
+                  }
+                )
+              })
+            }
+
+            let underline = document.getElementsByClassName('underline');
+
+            for (let i = 0; i < underline.length; i++) {
+              underline[i].addEventListener('mouseover', () => {
+                underline[i].animate(
+                  // keyframe
+                  [
+                    { transform: 'translateY(0)' },
+                    { transform: 'translateY(-3px)' },
+                    { transform: 'translateY(0)' }
+                  ], {
+                    // timing options
+                    duration: 600,
+                    iterations: 1
+                  }
+                )
+              });
+
+            }
+}
